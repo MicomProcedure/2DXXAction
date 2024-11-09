6ed51ec7f75ab6c53756dfc1602403bcdd49845c
mmm_yuto <manri.yuto.t8@dc.tohoku.ac.jp>
2024-11-09 14:51:34 +0900

    プレイヤーがランダムで変わるようにする

---
+++ Assets/Script/Player/PlayerChangeController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChangeController : MonoBehaviour
+{
+    // 子オブジェクトを格納するリスト
+    private List<GameObject> PlayerObjectList = new List<GameObject>();
+    //監督
+    [SerializeField] MusicController MusicController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // すべての子オブジェクトを取得してリストに追加
+        foreach (Transform child in transform)
+        {
+            PlayerObjectList.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //音楽が変わったタイミングを取得
+        if(MusicController.MusicLooped)
+        {
+            // アクティブなオブジェクトのインデックスを取得
+            int activeIndex = GetActiveObjectIndex(PlayerObjectList);
+            //アクティブ状態をオフにする
+            PlayerObjectList[activeIndex].SetActive(false);
+            //ランダムで次のキャラクターを選ぶ
+            while (true)
+            {
+                int randomValue = Random.Range(0,PlayerObjectList.Count);
+                //今と同じキャラでなければ抽選終了
+                if(randomValue != activeIndex)
+                {
+                    //新しいキャラクターを出す
+                    PlayerObjectList[randomValue].SetActive(true);
+                    //位置を調整
+                    PlayerObjectList[randomValue].transform.position = PlayerObjectList[activeIndex].transform.position;
+                    //速度情報の受け渡し
+                    PlayerObjectList[randomValue].GetComponent<Rigidbody2D>().velocity = PlayerObjectList[activeIndex].GetComponent<Rigidbody2D>().velocity;
+                    break;
+                }
+            }
+            MusicController.MusicLooped = false;
+        }
+    }
+
+    //何番目のオブジェクトがアクティブ状態かを調べる関数
+    int GetActiveObjectIndex(List<GameObject> objects)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (objects[i].activeSelf)  // アクティブ状態かを確認
+            {
+                return i;  // アクティブなオブジェクトのインデックスを返す
+            }
+        }
+        return -1;  // アクティブなオブジェクトが見つからない場合は -1 を返す
+    }
+}
